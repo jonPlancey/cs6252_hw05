1d28882e83301e8d44a7fc16461e13eedbaff682
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-08 00:27:44 -0500

    + added code that locates main.css

--- view/header.php
+++ view/header.php
@@ -1,17 +1,20 @@
-<?php
-	// determine the absolute path to the style sheet main.css
-	$uri = $_SERVER['REQUEST_URI'];
-	$dirs = explode('/', $uri);
-	$i = 1;
-	$path = '/';
+<?php 
+
+	$dir = ['/view/', '//../view/'];
 	
-	while ($dirs[$i] != "ch05_ex1") {
-		$path .= $dirs[$i] . '/';
-		$i += 1;
+	if (file_exists(dirname($dir[0]))) {
+		$path = $dir[0];
+	} else {
+		$path = $dir[1];
 	}
-	$path .= "ch05_ex1/";
-	echo(alert($path);)
-?>
+	
+	function doesFileExist(){
+		if (file_exists(dirname($dir[0]))) {
+	}
+
+?> 
+
+
 
 
 <!DOCTYPE html>
@@ -20,7 +23,7 @@
 	<head>
 		<title>My Guitar Shop</title>
 		<link rel="stylesheet" type="text/css"
-			  href="/ex_starts/ch05_ex1/main.css">
+			  href = "<?php echo $path ?>main.css">
 	</head>
 
 	<!-- the body section -->
