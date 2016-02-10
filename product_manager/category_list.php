a7cc2e6b4908a2dd87f763c8be6898426070a431
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-09 19:53:06 -0500

    + category_list, added code to build table with category, delete buttons

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -1,19 +1,45 @@
 <?php include '../view/header.php'; ?>
 <main>
-
     <h1>Category List</h1>
     <table>
         <tr>
             <th>Name</th>
             <th>&nbsp;</th>
-        </tr>        
-        <!-- add category rows here -->
+        </tr>
+        
+        <!-- Newly added table -->
+        
+		<?php foreach ($categories as $category) : ?>
+
+		<tr>                
+			<td><?php echo $category['categoryName']; ?></td>
+		  
+			<td><form action="delete_category.php" method="post">
+				<input type="hidden" name="category_id"  value="<?php echo $category['categoryID']; ?>">						 	
+				<input type="submit" value="Delete">
+			</form></td>
+		</tr>
+				
+		<?php endforeach; ?>  
+						 
+        <!-- Newly added tabe -->        
+    
     </table>
 
     <h2>Add Category</h2>
-    <!-- add code for form here -->
+    
+    <!-- Newly added form code -->   
+    <form action="add_category.php" method="post"> 
+	    <label>Name:</label>
+	    <input type="text" name="category_name">
+	
+	    <label>&nbsp;</label>
+	    <input type="submit" value="Add"><br>          
+    </form>
+    <!-- Newly added form code -->  
+    
+    <br>
+    <p><a href="index.php">List Products</a></p>
 
-    <p><a href="index.php?action=list_products">List Products</a></p>
-
-</main>
+    </main>
 <?php include '../view/footer.php'; ?>
